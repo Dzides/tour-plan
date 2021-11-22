724209c9e9e30d2bb60be523c9b4ae623e230ceb
Dzides <idzigun@.ru>
2021-11-23 01:08:50 +0300

    create: added switching Slider from the keyboard

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 1,
 
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+
     navigation: {
         nextEl: '.slider-button--next',
         prevEl: '.slider-button--prev',
