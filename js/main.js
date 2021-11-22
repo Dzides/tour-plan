e5c3ec204154fa67a243a6ed453ab5cdc4e1458b
Dzides <idzigun@.ru>
2021-11-23 00:40:48 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+
+    navigation: {
+        nextEl: '.slider-button--next',
+        prevEl: '.slider-button--prev',
+    },
+
+});
