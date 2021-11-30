0becf1dc459e8aca02cfb18377257b691e6daaee
Dzides <idzigun@.ru>
2021-12-01 00:55:56 +0300

    added mobile menu

--- js/main.js
+++ js/main.js
@@ -24,3 +24,8 @@ var swiperReviews = new Swiper('.reviews-slider', {
     },
 
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function() {
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
