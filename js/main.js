126d767810c4056a5a8fdabeeba7365e838ec5a3
Dzides <idzigun@.ru>
2021-12-06 23:51:58 +0300

    Added placeholger mask input

--- js/main.js
+++ js/main.js
@@ -79,5 +79,7 @@ $(document).ready(function () {
         });
     });
 
-    $('.phone').mask('+7 (000) 000-00-00');
+    $('.phone').mask('+7 (000) 000-00-00', {
+            placeholder: "+7 (___) ___-__-__"
+    });
 });
