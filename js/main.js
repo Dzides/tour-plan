12b5d90fe4f5121bede429e0f681526b6cf3ebb5
Dzides <idzigun@.ru>
2021-12-07 17:38:03 +0300

    Added hover, focus, active

--- js/main.js
+++ js/main.js
@@ -80,7 +80,5 @@ $(document).ready(function () {
         });
     });
 
-    $('.phone').mask('+7 (000) 000-00-00', {
-            placeholder: "+7 (___) ___-__-__*",
-    });
+    $('.phone').mask('+7 (000) 000-00-00');
 });
